efac1cb33ace1dbd650fef761e7596fb0d128876
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-02-15 11:12:49 +0700

    test deploy heroku

---
+++ Profile
@@ -0,0 +1,1 @@
+web: lend-borrow-book
--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"net/http"
 	"log"
 	"lend-borrow-book/routers"
+
+  "os"
 )
 
 func main() {
 	r := routers.New()
-	log.Fatal(http.ListenAndServe(":8080", r))
+
+  port := os.Getevn("PORT")
+  if (port == "") {
+    port = "8080"
+  }
+
+	log.Fatal(http.ListenAndServe(":" + port, r))
 }
