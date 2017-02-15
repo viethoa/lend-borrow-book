d38b490bde41fa5aa1e8bac282b3247438e4d48d
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-02-15 12:08:49 +0700

    using glide instead of Godeps

--- main.go
+++ main.go
@@ -4,17 +4,16 @@ import (
 	"net/http"
 	"log"
 	"lend-borrow-book/routers"
-
-  "os"
 )
 
 func main() {
 	r := routers.New()
 
-  port := os.Getevn("PORT")
-  if (port == "") {
-    port = "8080"
-  }
+  // port := os.Getevn("PORT")
+  // if (port == "") {
+  //   port = "8080"
+  // }
 
+  port := "8080"
 	log.Fatal(http.ListenAndServe(":" + port, r))
 }
