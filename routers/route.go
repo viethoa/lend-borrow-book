a058412507dc5adb3f6dd817a918a61132c80575
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-02-15 10:57:11 +0700

    fix package conflicts

--- routers/route.go
+++ routers/route.go
@@ -2,11 +2,7 @@ package routers
 
 import(
 	"gopkg.in/gin-gonic/gin.v1"
-<<<<<<< HEAD
-	"LendBorrowBook/handlers/users"
-=======
 	"lend-borrow-book/handlers/users"
->>>>>>> e8356589b73a03c327f077772797619565dbce70
 )
 
 func New() *gin.Engine {
