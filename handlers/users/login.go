c3d7465cb8b91dc78b887f2b5fe14e8cdcc7e720
Khuat Viet Hoa <hoa.khuatviet@gmail.com>
2017-02-15 10:25:11 +0700

    setup heroku repo

--- handlers/users/login.go
+++ handlers/users/login.go
@@ -6,5 +6,16 @@ import(
 )
 
 func Login(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+  email := c.Param("email")
+  password := c.Param("password")
+  if (len(email) == 0) {
+	   c.JSON(http.StatusOK, gin.H{"message": "missing email"})
+     return
+  }
+  if (len(password) == 0) {
+     c.JSON(http.StatusOK, gin.H{"message": "missing password"})
+     return
+  }
+
+  c.JSON(http.StatusOK, gin.H{"message": "okie"})
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 	"log"
-	"LandBorrowBook/routers"
+	"LendBorrowBook/routers"
 )
 
 func main() {
